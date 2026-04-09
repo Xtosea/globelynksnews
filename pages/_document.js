528ac0bfa19882746d48c12cc1c2e44d43660e5d
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 18:28:25 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,18 +5,16 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Adcash Library */}
-       <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
-
-<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
-
-
+        <script
+          id="aclib"
+          type="text/javascript"
+          src="//acscdn.com/script/aclib.js"
+        ></script>
       </Head>
 
       <body className="bg-white dark:bg-gray-900 transition-colors">
         <Main />
         <NextScript />
-
-        
       </body>
     </Html>
   );
