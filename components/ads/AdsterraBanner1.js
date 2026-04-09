1bd7cdfcfe7ea77b8c4c9bfa18ed25b1320dabf9
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 19:25:37 +0100

    Update AdsterraBanner1.js

--- components/ads/AdsterraBanner1.js
+++ components/ads/AdsterraBanner1.js
@@ -1,24 +1,38 @@
+"use client";
 
+import { useEffect } from "react";
 
+export default function TrendingAd() {
+  useEffect(() => {
+    // Prevent duplicate load
+    if (document.getElementById("trend-ad-script")) return;
 
+    // Define global options
+    window.atOptions = {
+      key: "1f0fc51684d9eb27e09a097d7db65457",
+      format: "iframe",
+      height: 250,
+      width: 300,
+      params: {},
+    };
 
+    const script = document.createElement("script");
+    script.id = "trend-ad-script";
+    script.async = true;
+    script.src =
+      "https://www.highperformanceformat.com/1f0fc51684d9eb27e09a097d7db65457/invoke.js";
 
+    document.body.appendChild(script);
+  }, []);
 
-
-
-
-
-
-src="https://www.highperformanceformat.com/1f0fc51684d9eb27e09a097d7db65457/invoke.js"
-
-
-<script>
-  atOptions = {
-    'key' : '1f0fc51684d9eb27e09a097d7db65457',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-</script>
-<script src="https://www.highperformanceformat.com/1f0fc51684d9eb27e09a097d7db65457/invoke.js"></script>
+  return (
+    <div
+      style={{
+        width: "300px",
+        height: "250px",
+        margin: "20px auto",
+        textAlign: "center",
+      }}
+    />
+  );
+}
