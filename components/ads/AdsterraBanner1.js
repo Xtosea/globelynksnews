14a52f965dc7f1bfac8fd9cf329dcd4acb0132ab
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 19:09:03 +0100

    Create AdsterraBanner1.js

---
+++ components/ads/AdsterraBanner1.js
@@ -0,0 +1,10 @@
+<script>
+  atOptions = {
+    'key' : '1f0fc51684d9eb27e09a097d7db65457',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/1f0fc51684d9eb27e09a097d7db65457/invoke.js"></script>
