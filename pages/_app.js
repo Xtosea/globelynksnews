55ab3d111660604aecd46fad387241419d2e7e4f
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 06:43:57 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
-import Head from "next/head"
+// pages/_app.js
+import Head from "next/head";
 import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>Globelynks News</title>
+
         {/* Tailwind CDN */}
         <script src="https://cdn.tailwindcss.com"></script>
 
         {/* Monetad Ad Script */}
-       
-       <Layout>
-      <Component {...pageProps} />
-    </Layout>
-
         <script
           src="https://5gvci.com/act/files/tag.min.js?z=10851573"
           data-cfasync="false"
@@ -37,13 +36,16 @@ export default function App({ Component, pageProps }) {
           }}
         />
 
-        {/* SEO */}
-        <meta property="og:type" content="article" />
-        <meta property="og:site_name" content="Globelynks Blog" />
+        {/* SEO Defaults */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Globelynks News" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      <Component {...pageProps} />
+      {/* Wrap all pages with Layout */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
-  )
+  );
 }
