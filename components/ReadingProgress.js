8acd235bf75c04513face51c4054ace292d5a434
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 12:21:03 +0100

    Create ReadingProgress.js

---
+++ components/ReadingProgress.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ReadingProgress() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const scrollPosition = window.scrollY;
+
+      const scrollPercent = (scrollPosition / totalHeight) * 100;
+
+      setProgress(scrollPercent);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div
+      className="fixed top-0 left-0 h-1 bg-red-600 z-50 transition-all duration-200"
+      style={{ width: `${progress}%` }}
+    />
+  );
+}
