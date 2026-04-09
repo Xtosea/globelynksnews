e9c61a2392566caaa6559ba78af8673b2d03e3bc
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 18:29:01 +0100

    Update AdcashVideoSlider.js

--- components/AdcashVideoSlider.js
+++ components/AdcashVideoSlider.js
@@ -1,12 +1,20 @@
+"use client";
+
 import { useEffect } from "react";
 
 export default function AdcashVideoSlider() {
   useEffect(() => {
-    if (window.aclib) {
-      window.aclib.runVideoSlider({
-        zoneId: "11175702",
-      });
-    }
+    const interval = setInterval(() => {
+      if (window.aclib) {
+        window.aclib.runVideoSlider({
+          zoneId: "11175702",
+        });
+
+        clearInterval(interval);
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
   }, []);
 
   return null;
