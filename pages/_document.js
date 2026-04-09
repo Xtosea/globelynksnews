c54feeba368aa732983427575d36f9cf48c7a19e
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-09 13:10:19 +0100

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+
+      <body className="bg-white dark:bg-gray-900 transition-colors">
+        <Main />
+        <NextScript />
+
+     <script type="text/javascript">
+    aclib.runAutoTag({
+        zoneId: '0vlavhw2a',
+    });
+</script>
+
+
+      </body>
+
+    </Html>
+  );
+}
